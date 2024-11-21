8fd1b67fbd63db92d54abf3e380156f729209443
Slava Mokrynskyi <chempionslava@gmail.com>
2024-11-21 19:42:16 +0200

    Add project files.

---
+++ OOP-003/OOP-003.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class String
+{
+	char* str;
+	int size;
+public:
+	static int count;
+	String(int size, bool isNeedToBeInitialized)
+	{
+		this->size = size;
+		this->str = new char[size];
+		if (isNeedToBeInitialized == true)
+		{
+			cout << "Enter your string -> ";
+			cin.getline(this->str, size);
+		}
+		count++;
+	}
+	String(int size) : String(size, false) { count++; }
+	String() : String(80, false) { count++; }
+	void Show(String* str)
+	{
+		cout << "Yout string: \n" << str->str << "\n";
+	}
+	~String()
+	{
+		delete[] str;
+		count--;
+	}
+};
+
+int String::count = 0;
+
+int main()
+{
+	String str1(100, true);
+	str1.Show(&str1);
+}
